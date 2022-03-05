b3f48d60826521c49e9f0de94b272453c8a6c909
Derrick Hammer <derrick@derrickhammer.com>
2022-03-05 17:24:06 -0500

    *Add unit tests

---
+++ tests/resolvers/ens.ts
@@ -0,0 +1,25 @@
+import resolver from "../../src/index.js";
+import Eip137 from "../../src/resolvers/eip137.js";
+import is_ipfs from "is-ipfs";
+import assert from "assert";
+
+const {ipnsPath} = is_ipfs;
+
+resolver.registerPortal(<string>process.env.TEST_PORTAL);
+
+describe('Eip137', () => {
+    const subresolver = new Eip137(resolver);
+    it('should resolve ens.eth correctly', async () => {
+        // @ts-ignore
+        let result = <string>await subresolver.resolve('ens.eth');
+        assert.notEqual(result, null);
+        assert.notEqual(result, '');
+        let path = `/${result.replace("://", "/")}`;
+        assert.equal(ipnsPath(path), true);
+    });
+    it('should fail to resolve lumeweb.eth correctly', async () => {
+        // @ts-ignore
+        let result = <string>await subresolver.resolve('lumeweb.eth');
+        assert.equal(result, false);
+    });
+})
