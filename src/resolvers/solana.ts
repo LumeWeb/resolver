aeaa7209239b562c9a8a482d904174798ffb598a
Derrick Hammer <derrick@derrickhammer.com>
2022-04-30 01:52:21 -0400

    *Handle protocol identifier with = as well as normal url protocol

--- src/resolvers/solana.ts
+++ src/resolvers/solana.ts
@@ -46,9 +46,19 @@ export default class Solana extends SubResolverBase {
 
     res.data = nameAccount.data?.slice(NameRegistryState.HEADER_LEN);
 
-    const content = res.data.toString("ascii").replace(/\0/g, "");
+    let content = res.data.toString("ascii").replace(/\0/g, "");
 
-    const skylink = await normalizeSkylink(content, this.resolver);
+    let skylink = await normalizeSkylink(content, this.resolver);
+
+    if (skylink) {
+      return skylink;
+    }
+
+    if (content.includes("=")) {
+      content = content.split("=")[0];
+    }
+
+    skylink = await normalizeSkylink(content, this.resolver);
 
     if (skylink) {
       return skylink;
